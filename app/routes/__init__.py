abc444900f94efb2df5e330d5b4eabcd551c7290
fushang318 <fushang318@gmail.com>
2021-08-11 17:58:19 +0800

    feat: change jobs query api and daos query api

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -20,7 +20,8 @@ class Query(ObjectType):
         sorted_type=DAOsSortedTypeEnum(),
         search=String(),
         first=Int(default_value=20),
-        offset=Int(default_value=0)
+        offset=Int(default_value=0),
+        user_name=String()
     )
 
     dao = Field(
@@ -53,7 +54,7 @@ class Query(ObjectType):
         sorted_type=SortedTypeEnum(),
         first=Int(default_value=20),
         offset=Int(default_value=0),
-        user_id=String(),
+        user_name=String(),
     )
 
     @staticmethod
