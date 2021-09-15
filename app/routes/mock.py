c9b92d18755f34aaa63076dcc0c88a6416734db5
bqx619 <bqx619@gmail.com>
2021-09-15 20:51:20 +0800

    feat: add mint mock

--- app/routes/mock.py
+++ app/routes/mock.py
@@ -1,6 +1,8 @@
 from graphene import Mutation, String, Boolean
 
+from app.common.utils.errors import COMMON_NOT_PERMISSION_ERROR
 from app.controllers.mock import init_mock_data
+from settings import ICPDAO_APP_ENV
 
 
 class CreateMock(Mutation):
@@ -11,6 +13,7 @@ class CreateMock(Mutation):
     ok = Boolean()
 
     def mutate(self, info, owner_github_user_login, icpper_github_user_login):
+        assert ICPDAO_APP_ENV != 'PROD', COMMON_NOT_PERMISSION_ERROR
         background_tasks = info.context['background']
         background_tasks.add_task(init_mock_data, owner_github_user_login, icpper_github_user_login)
 
